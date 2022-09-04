b7b38238efd98ab4fbdd7de566d505f0e89ae373
Yash Jambhale <81085813+YashJ-27@users.noreply.github.com>
2022-09-04 13:03:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css'
+import Home from './Home'
+import Header from './Header'
+
+
+function App() {
+  return (
+    <div className="app">
+      
+      <Header />
+      <Home />
+
+      {/* Home */}
+        {/* Header */}
+
+        {/* Banner */}
+
+          {/* Search */}
+        {/* Cards */}
+
+        {/* Footer */}
+      
+      {/* Search Page*/}
+        {/* ... */}
+
+    </div>
+  );
+}
+
+export default App;
